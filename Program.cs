3bd5cca0445d454c4e5b53af20d3a2a5d02fce0c
Yuriy <aviaprad@bk.ru>
2023-06-30 13:46:52 +0300

    Add new file with program Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// 
+string[] array = new string[] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+
+int lengthNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) lengthNewArray++;
+}
+
+string[] newArray = new string[lengthNewArray];
+int index = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[index] = array[i];
+        index++;
+    }
+}
+
+void PrintArray(string[] array) // метод печати массива
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
